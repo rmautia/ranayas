dc5d5b4340367ce85e3c47b795ec2cbc3f6012ca
rmautia <raphaelnyangenya@gmail.com>
2020-03-01 19:43:33 +0300

    add specials to page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
-    $("#topcont .chef").hover(function(){
-        $(this).find(".pic").css("pic","flex");
-    },function(){
-        $(this).find(".pic").hide();
-    });
+
 });
