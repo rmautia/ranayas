35a00984a9cde208e8fd0b5e32959e5dd494de7b
rmautia <raphaelnyangenya@gmail.com>
2020-03-01 21:13:47 +0300

    add .offer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 
+    $("#menu .column1").hover(function(){
+        $(this).find(".offer").css("display","flex");
+    },function(){
+        $(this).find(".offer").hide();
+    });
 });
