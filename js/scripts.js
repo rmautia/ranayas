869f9ce322d2abef4f8b11277683b6272edce259
rmautia <raphaelnyangenya@gmail.com>
2020-03-01 21:45:34 +0300

    add image pz.jpg.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $("#menu .column1").hover(function(){
+    $("#menu .col-md-3").hover(function(){
         $(this).find(".offer").css("display","flex");
     },function(){
         $(this).find(".offer").hide();
